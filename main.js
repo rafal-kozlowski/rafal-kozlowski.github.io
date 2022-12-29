68083b6bff6452902efae819cbc8b63be4d74483
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2022-12-29 20:13:25 +0100

    add content hiding buttons in main sections, add contact-style.css

--- main.js
+++ main.js
@@ -1,121 +1,3 @@
-//========== contact hiding button ==========//
-let contactShow = document.getElementById('contact-show');
-let contactHide = document.getElementById('contact-hide');
-let contactContent = document.getElementById('contact-content');
-
-contactShow.style.display = 'none'; 
-
-function hideContactContent() {
-    contactContent.style.display = 'none';
-    contactShow.style.display = ''; 
-    contactHide.style.display = 'none'; 
-}
-
-contactHide.addEventListener('click', hideContactContent);
-
-const showContactContent = () => {
-    contactContent.style.display = '';
-    contactShow.style.display = 'none'; 
-    contactHide.style.display = ''; 
-};
-contactShow.onclick = showContactContent;
-
-//========== software&languages hiding button ==========//
-
-let softwareSkillShow = document.getElementById('software-skill-show');
-let softwareSkillHide = document.getElementById('software-skill-hide');
-let softwareSkillContent = document.getElementById('software-skill-content');
-
-softwareSkillShow.style.display = 'none';
-
-const hideSoftwareSkillContent = () => {
-    softwareSkillContent.style.display ='none';
-    softwareSkillHide.style.display = 'none';
-    softwareSkillShow.style.display = '';
-};
-
-softwareSkillHide.onclick = hideSoftwareSkillContent;
-
-const showSoftwareSkillContent = () => {
-    softwareSkillContent.style.display = '';
-    softwareSkillHide.style.display = '';
-    softwareSkillShow.style.display = 'none';
-};
-
-softwareSkillShow.onclick = showSoftwareSkillContent;
-
-//========== soft skill hiding button ==========//
-
-let softSkillShow = document.getElementById('soft-skill-show');
-let softSkillHide = document.getElementById('soft-skill-hide');
-let softSkillContent = document.getElementById('soft-skill-content');
-
-softSkillShow.style.display = 'none';
-
-const hideSoftSkillContent = () => {
-    softSkillContent.style.display ='none';
-    softSkillHide.style.display = 'none';
-    softSkillShow.style.display = '';
-};
-
-softSkillHide.onclick = hideSoftSkillContent;
-
-const showSoftSkillContent = () => {
-    softSkillContent.style.display = '';
-    softSkillHide.style.display = '';
-    softSkillShow.style.display = 'none';
-};
-
-softSkillShow.onclick = showSoftSkillContent;
-
-//========== additional activities hiding button ==========//
-
-let activitiesShow = document.getElementById('activities-show');
-let activitiesHide = document.getElementById('activities-hide');
-let activitiesContent = document.getElementById('activities-content');
-
-activitiesShow.style.display = 'none';
-
-const hideActivitiesContent = () => {
-    activitiesContent.style.display ='none';
-    activitiesHide.style.display = 'none';
-    activitiesShow.style.display = '';
-};
-
-activitiesHide.onclick = hideActivitiesContent;
-
-const showActivitiesContent = () => {
-    activitiesContent.style.display = '';
-    activitiesHide.style.display = '';
-    activitiesShow.style.display = 'none';
-};
-
-activitiesShow.onclick = showActivitiesContent;
-
-//========== interest hiding button ==========//
-
-let interestShow = document.getElementById('interest-show');
-let interestHide = document.getElementById('interest-hide');
-let interestContent = document.getElementById('interest-content');
-
-interestShow.style.display = 'none';
-
-const hideInterestContent = () => {
-    interestContent.style.display ='none';
-    interestHide.style.display = 'none';
-    interestShow.style.display = '';
-};
-
-interestHide.onclick = hideInterestContent;
-
-const showInterestContent = () => {
-    interestContent.style.display = '';
-    interestHide.style.display = '';
-    interestShow.style.display = 'none';
-};
-
-interestShow.onclick = showInterestContent;
-
 //========== education hiding button ==========//
 
 let educationShow = document.getElementById('education-show');
@@ -140,7 +22,62 @@ const showEducationContent = () => {
 
 educationShow.onclick = showEducationContent;
 
-//========== emplyment hiding button ==========//
+
+//========== skills hiding button ==========//
+
+let skillShow = document.getElementById('software-skill-show');
+let skillHide = document.getElementById('software-skill-hide');
+let skillContent = document.getElementById('skills-grid-container');
+
+skillShow.style.display = 'none';
+
+const hideSkillContent = () => {
+    skillContent.style.display ='none';
+    skillHide.style.display = 'none';
+    skillShow.style.display = '';
+};
+
+skillHide.onclick = hideSkillContent;
+
+const showSkillContent = () => {
+    skillContent.style.display = '';
+    skillHide.style.display = '';
+    skillShow.style.display = 'none';
+};
+
+skillShow.onclick = showSkillContent;
+
+//========== projects hiding button ==========//
+
+let show = document.getElementById('show-button');
+let hide = document.getElementById('hide-button');
+let projectsContent = document.getElementById('projects-content');
+
+show.style.display = 'none';
+
+const hideButtonFire = () => {
+    hide.style.display = 'none';
+    show.style.display = '';
+}
+
+const hideProjectsContent = () => {
+    projectsContent.style.display = 'none';
+}
+
+hide.addEventListener('click', () => {hideProjectsContent(); hideButtonFire()});
+
+const showButtonFire = () => {
+    show.style.display = 'none';
+    hide.style.display = '';
+}
+
+const showProjectsContent = () => {
+    projectsContent.style.display = '';
+}
+
+show.addEventListener('click', () => {showProjectsContent(); showButtonFire()});
+
+//========== employment hiding button ==========//
 
 let employmentShow = document.getElementById('employment-show');
 let employmentHide = document.getElementById('employment-hide');
@@ -163,3 +100,27 @@ const showEmploymentContent = () => {
 };
 
 employmentShow.onclick = showEmploymentContent;
+
+//========== interest hiding button ==========//
+
+let interestShow = document.getElementById('interest-show');
+let interestHide = document.getElementById('interest-hide');
+let interestContent = document.getElementById('interest-content');
+
+interestShow.style.display = 'none';
+
+const hideInterestContent = () => {
+    interestContent.style.display ='none';
+    interestHide.style.display = 'none';
+    interestShow.style.display = '';
+};
+
+interestHide.onclick = hideInterestContent;
+
+const showInterestContent = () => {
+    interestContent.style.display = '';
+    interestHide.style.display = '';
+    interestShow.style.display = 'none';
+};
+
+interestShow.onclick = showInterestContent;
